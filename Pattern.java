7ce7f0442a4261e525100d2010ca534ec6e58633
poorvautturkar25 <poorvautturkar05@gmail.com>
2025-06-24 12:54:00 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+package com.dkte;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i,j, n=5;
+    
+    for(i=n;i>=1;i--) {
+    	
+    	for(j=1;j <= i;j++) {
+    		System.out.print("*");
+    	}
+    	System.out.println();
+    		
+    }
+	}
+
+}
